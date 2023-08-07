2b92980cc115a96841c056b4032efb3d7119044a
zzr23 <134657900+zzr23@users.noreply.github.com>
2023-08-06 23:48:18 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,60 +1,55 @@
 
-letters = "abcdefghijklmnopqrstuvwxyz'
-num_letter = len(letters)
+letters = "abcdefghijklmnopqrstuvwxyz"
+num_letters = len(letters)
 
 def encrypt(plaintext, key):
     ciphertext = ''
     for letter in plaintext:
         letter = letter.lower()
-        if not letter == ' ':
-          index = letters.find(letter)
-          if index == -1:
-            ciphertext += letter
-          else:
-            new_index = index + key
-            if new_index >= num_letter:
-              new_index -= num_letter
-            ciphertext += letters[new_index]
+        if letter != ' ':
+            index = letters.find(letter)
+            if index == -1:
+                ciphertext += letter
+            else:
+                new_index = (index + key) % num_letters
+                ciphertext += letters[new_index]
+        else:
+            ciphertext += ' '
     return ciphertext
-  
-  def decrypt(ciphertext, key):
+
+def decrypt(ciphertext, key):
     plaintext = ''
     for letter in ciphertext:
         letter = letter.lower()
-        if not letter == ' ':
-          index = letters.find(letter)
-          if index == -1:
-            plaintext += letter
-          else:
-            new_index = index - key
-            if new_index < 0:
-              new_index += num_letter
-            plaintext += letters[new_index]
+        if letter != ' ':
+            index = letters.find(letter)
+            if index == -1:
+                plaintext += letter
+            else:
+                new_index = (index - key) % num_letters
+                plaintext += letters[new_index]
+        else:
+            plaintext += ' '
     return plaintext
-    
+
 def encrypt_decrypt(text, mode, key):
     result = ''
     if mode == 'd':
-      key = -key
+        key = -key
 
     for letter in text:
-      letter = letter.lower()
-      if not letter == ' ':
-          index = letters.find(letter)
-          if index == -1
-              result += letter
+        letter = letter.lower()
+        if letter != ' ':
+            index = letters.find(letter)
+            if index == -1:
+                result += letter
+            else:
+                new_index = (index + key) % num_letters
+                result += letters[new_index]
         else:
-            new_index = index + key
-            if new_index >= num_letters
-                new_index -= num_letters
-            elif new_index < 0:
-                new_index += num_letters
-            result += letters [new_index]
-      return result
+            result += ' '
+    return result
 
-
-
-print()
 print('*** CAESAR CIPHER PROGRAM ***')
 print()
 
@@ -62,18 +57,18 @@ print('Do you want to encrypt or decrypt?')
 user_input = input('e/d: ').lower()
 print()
 
-if user_input == 'e'
-  print('ENCRYPTION MODE SELECTED')
-  print()
-  key = int(input('Enter the key (1 through 26): '))
-  text = input('Enter the text to encrypt: ')
-  ciphertext = encrypt_decrypt(text, user_input, key)
-  print(f'CIPHERTEXT: {ciphertext} ')
+if user_input == 'e':
+    print('ENCRYPTION MODE SELECTED')
+    print()
+    key = int(input('Enter the key (1 through 26): '))
+    text = input('Enter the text to encrypt: ')
+    ciphertext = encrypt_decrypt(text, user_input, key)
+    print(f'CIPHERTEXT: {ciphertext}')
 
 elif user_input == 'd':
-  print('DECRYPTION MODE SELECTED')
-  print()
-  key = int(input(' Enter the key (1 through 26): '))
-  text = input('Enter the text to decrypt: ')
-  plaintext = encrypt_decrypt(text, user_input, key)
-  print(f'PLAINTEXT: {plaintext} ')
+    print('DECRYPTION MODE SELECTED')
+    print()
+    key = int(input('Enter the key (1 through 26): '))
+    text = input('Enter the text to decrypt: ')
+    plaintext = encrypt_decrypt(text, user_input, key)
+    print(f'PLAINTEXT: {plaintext}')
